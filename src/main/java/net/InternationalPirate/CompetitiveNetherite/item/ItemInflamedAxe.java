319fbf3e83804195d5ff17e06586070d13bd90f3
InternationalPirate <the.mlg.carrot@gmail.com>
2024-03-04 09:28:21 -0700

    hoe is op

--- src/main/java/net/InternationalPirate/CompetitiveNetherite/item/ItemInflamedAxe.java
+++ src/main/java/net/InternationalPirate/CompetitiveNetherite/item/ItemInflamedAxe.java
@@ -1,6 +1,10 @@
 package net.InternationalPirate.CompetitiveNetherite.item;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.*;
+import net.minecraft.world.entity.monster.hoglin.Hoglin;
+import net.minecraft.world.entity.monster.piglin.Piglin;
+import net.minecraft.world.entity.monster.piglin.PiglinBrute;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
@@ -13,6 +17,41 @@ public class ItemInflamedAxe extends AxeItem {
 
     @Override
     public boolean hurtEnemy(@NotNull ItemStack pStack, LivingEntity pTarget, @NotNull LivingEntity pAttacker) {
+        if (pTarget instanceof Blaze) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof MagmaCube) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof Piglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            pTarget.setSecondsOnFire(11);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof PiglinBrute) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            pTarget.setSecondsOnFire(11);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof Hoglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            pTarget.setSecondsOnFire(11);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof ZombifiedPiglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof Zoglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof WitherSkeleton) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 32);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
         pTarget.setSecondsOnFire(11);
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
--- src/main/java/net/InternationalPirate/CompetitiveNetherite/item/ItemInflamedSword.java
+++ src/main/java/net/InternationalPirate/CompetitiveNetherite/item/ItemInflamedSword.java
@@ -1,6 +1,10 @@
 package net.InternationalPirate.CompetitiveNetherite.item;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.*;
+import net.minecraft.world.entity.monster.hoglin.Hoglin;
+import net.minecraft.world.entity.monster.piglin.Piglin;
+import net.minecraft.world.entity.monster.piglin.PiglinBrute;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -12,7 +16,42 @@ public class ItemInflamedSword extends SwordItem {
     }
 
     @Override
-    public boolean hurtEnemy(@NotNull ItemStack pStack, LivingEntity pTarget, @NotNull LivingEntity pAttacker) {
+    public boolean hurtEnemy(@NotNull ItemStack pStack, @NotNull LivingEntity pTarget, @NotNull LivingEntity pAttacker) {
+        if (pTarget instanceof Blaze) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof MagmaCube) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof Piglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            pTarget.setSecondsOnFire(11);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof PiglinBrute) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            pTarget.setSecondsOnFire(11);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof Hoglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            pTarget.setSecondsOnFire(11);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof ZombifiedPiglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof Zoglin) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
+        if (pTarget instanceof WitherSkeleton) {
+            pTarget.hurt(pAttacker.level().damageSources().generic(), 30);
+            return super.hurtEnemy(pStack, pTarget, pAttacker);
+        }
         pTarget.setSecondsOnFire(11);
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
